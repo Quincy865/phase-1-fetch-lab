8e792a109a30cdaffb7288c6f97996bc970b20dd
Quincy Michael <michaelkirima865@gmail.com>
2024-10-15 10:43:04 +0300

    Fetch lab

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json()) 
+      .then(data => renderBooks(data))  
+      .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const bookList = document.getElementById('book-list');
+  
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+      const li = document.createElement('li');
+      li.textContent = book.name; 
+      bookList.appendChild(li);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
